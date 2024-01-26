80adfd64346aa6f723ecdca9f188dc6894ad4463
unknown <aguit.inan@gmail.com>
2024-01-26 13:48:14 +0100

    GREEN 2

--- TDD-DictionaryCore/Class1.cs
+++ TDD-DictionaryCore/Class1.cs
@@ -4,8 +4,17 @@ namespace TDD_DictionaryCore
 {
     public class StringReplacer
     {
+        //public static string ReplaceString(string input, Dictionary<string, string> replacements)
+        //{
+        //    return input;
+        //}
+
         public static string ReplaceString(string input, Dictionary<string, string> replacements)
         {
+            foreach (var pair in replacements)
+            {
+                input = input.Replace($"${pair.Key}$", pair.Value);
+            }
             return input;
         }
     }
