9301a753ea441231a6b49d7791767c10bd5627f3
unknown <aguit.inan@gmail.com>
2024-01-26 13:55:32 +0100

    RED 3

--- TDD-DictionaryTests/UnitTest1.cs
+++ TDD-DictionaryTests/UnitTest1.cs
@@ -25,5 +25,18 @@ namespace TDD_DictionaryTests
             string result = StringReplacer.ReplaceString(input, dict);
             Assert.AreEqual("temporaire", result);
         }
+
+        [TestMethod]
+        public void ReplaceString_WithMultipleReplacements_ReturnsReplacedString()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "temp", "temporary" },
+                { "name", "John Doe" }
+            };
+            string input = "$temp$ here comes the name $name$";
+            string result = StringReplacer.ReplaceString(input, dict);
+            Assert.AreEqual("temporary here comes the name John Doe", result);
+        }
     }
 }
